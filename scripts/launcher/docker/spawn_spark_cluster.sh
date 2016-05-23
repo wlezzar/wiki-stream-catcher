61c7dad6943d1269d5ad8a05b027a8357285a03c
Walid Lezzar <walid.lezzar@gmail.com>
2016-05-23 10:43:42 +0200

    [DOCKER] using the new docker network features instead of weave

--- scripts/launcher/docker/spawn_spark_cluster.sh
+++ scripts/launcher/docker/spawn_spark_cluster.sh
@@ -1,28 +1,21 @@
-# Install weave : 
-sudo curl -L git.io/weave -o /usr/local/bin/weave
-sudo chmod a+x /usr/local/bin/weave
-
-# Each time a new shell is opened : 
-weave launch
-eval $(weave env)
+# Create the network
+docker network create spark_cluster
 
 # Launch the master
-docker run --rm -p 8080:8080 -it -e SPARK_MASTER_IP=master --name master yaronr/spark
-echo 'SPARK_MASTER_IP=master' > ../conf/spark-env.sh && ../sbin/start-master.sh
-tail -f /usr/spark-1.5.2-bin-hadoop2.6/logs/spark-*
+docker run --rm -p 8080:8080 -it -e SPARK_MASTER_IP=master --name master --net=spark_cluster yaronr/spark
+../sbin/start-master.sh && tail -f /usr/spark-1.5.2-bin-hadoop2.6/logs/spark-*
 
 # Launch the slaves
-docker run --rm -it --name slave01 yaronr/spark
-docker run --rm -it --name slave02 yaronr/spark
-docker run --rm -it --name slave03 yaronr/spark
-docker run --rm -it --name slave04 yaronr/spark
+docker run --rm -it --name slave01 --net=spark_cluster yaronr/spark
+docker run --rm -it --name slave02 --net=spark_cluster yaronr/spark
+docker run --rm -it --name slave03 --net=spark_cluster yaronr/spark
+docker run --rm -it --name slave04 --net=spark_cluster yaronr/spark
 
 # For each worker, execute this : 
 ../sbin/start-slave.sh spark://master:7077 -c 1 -m 1g && tail -f /usr/spark-1.5.2-bin-hadoop2.6/logs/spark-*
 
 # Optional : to open a driver
-eval $(weave env)
-docker run --rm -p 4041:4040 -it --name driver yaronr/spark
+docker run --rm -p 4041:4040 -it --name driver --net=spark_cluster yaronr/spark
 ./spark-shell --master spark://master:7077
 sc.parallelize(1 to 1000).groupBy(_ % 2 == 0).mapValues(_.reduce(_+_)).collect
 
